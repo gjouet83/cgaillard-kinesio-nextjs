dc6939876a054630d91527536ff28bf2745e95da
gjouet83 <jouetguillaume@gmail.com>
2024-07-29 18:29:59 +0200

    init project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+// Définit __dirname pour les modules ES
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
 
 export default nextConfig;
